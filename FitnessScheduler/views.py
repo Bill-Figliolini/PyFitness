d56f894f1a9c89e32be304cf89bad146e18d8b35
Bill Figliolini <BillFigliolini@gmail.com>
2025-08-25 18:19:09 -0400

    Beginning Front-end work

--- FitnessScheduler/views.py
+++ FitnessScheduler/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World, Welcome to fitness Tracker")
+
+
+def login(request) -> HttpResponse:
+    return HttpResponse("TODO - Login")
+
+
+def userDashboard(request) -> HttpResponse:
+    return HttpResponse("TODO - Dashboard")
+
+
+def planner(request) -> HttpResponse:
+    return HttpResponse("TODO - Planner")
+
+
+def recordPage(request) -> HttpResponse:
+    return HttpResponse("TODO - Records")
