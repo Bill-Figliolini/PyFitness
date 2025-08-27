17cb77369b61213dbe554c3e1bd0fe01762dce20
Bill Figliolini <BillFigliolini@gmail.com>
2025-08-27 17:27:07 -0400

    moved the useraccount model into the accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
+from typing import override
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.fields.related import OneToOneField
+
 
 # Create your models here.
+class UserAccount(models.Model):
+    """
+    Class for handling the Database potion of userdata.
+
+    Attributes:
+    Methods:
+    """
+
+    account_binding: OneToOneField[User] = models.OneToOneField(
+        User, on_delete=models.CASCADE
+    )
+
+    @override
+    def __str__(self) -> str:
+        return f"User {self.account_binding}"
