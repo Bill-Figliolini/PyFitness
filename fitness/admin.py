b307e49da622bc1b1ed5b4f52ce17c36a3442994
Bill Figliolini <BillFigliolini@gmail.com>
2025-09-16 17:39:51 -0400

    Registered user model with fitness app

--- fitness/admin.py
+++ fitness/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User
 from fitness.models import Exercise
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
 admin.site.register(Exercise)
