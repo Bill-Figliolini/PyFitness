653fc5f6109709fb26bab54d057472d5d430f430
Bill Figliolini <BillFigliolini@gmail.com>
2025-09-16 16:38:48 -0400

    removed references to default django user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,11 @@
 from typing import override
-from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.fields import IntegerField, TextField
 from django.db.models.fields.related import OneToOneField
 
 
 # Create your models here.
-class UserAccount(models.Model):
+class User(models.Model):
     """
     Class for handling the Database potion of userdata.
 
@@ -15,9 +14,6 @@ class UserAccount(models.Model):
     """
 
     id: IntegerField[int] = models.IntegerField(primary_key=True)
-    account_binding: OneToOneField[User] = models.OneToOneField(
-        User, on_delete=models.CASCADE
-    )
     name: TextField[str] = TextField()
 
     @override
