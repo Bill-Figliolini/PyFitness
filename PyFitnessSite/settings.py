9eeadec9c918fa5bfe661778c1ba5380fe28284b
Bill Figliolini <BillFigliolini@gmail.com>
2025-09-17 17:46:00 -0400

    Updated db to use PostGreSQL

--- PyFitnessSite/settings.py
+++ PyFitnessSite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "PyFitnessSite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "PyFitnessDB",
+        "USER": "PyFitnessDBAdmin",
+        "PASSWORD": "TempDBPass",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
